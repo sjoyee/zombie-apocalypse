36c9aeb90f2c32ed47680045ed3bdf9287f74f34
Jo Yee <jsia0008@student.monash.edu>
2020-05-17 22:28:35 +0800

    Modified Food class to allow Player to eat food (Farmers and food)

--- src/game/Food.java
+++ src/game/Food.java
@@ -5,7 +5,7 @@ public class Food extends PortableItem{
     public Food() {
         super("food", 'o');
         addCapability(ItemCapability.EDIBLE);
-//        allowableActions.add(new EatAction(this));
+        allowableActions.add(new EatAction(this));
     }
 }
 
