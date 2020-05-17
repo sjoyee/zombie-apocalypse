59222f572d5d512b11a48f832a213726f17c3da0
Lua Shi Liang <slua0003@student.monash.edu>
2020-05-17 22:14:03 +0800

    ZombieClub class added

---
+++ src/game/ZombieClub.java
@@ -0,0 +1,11 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieClub extends WeaponItem{
+
+	public ZombieClub() {
+		super("ZombieClub", 'T', 40, "ZombieClub whacks");
+	}
+
+}
