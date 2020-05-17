7310ca28ea04301a8c9ad0e71ba2fbdd58dfdd37
Lua Shi Liang <slua0003@student.monash.edu>
2020-05-17 22:12:47 +0800

    ZombieMace Class added

---
+++ src/game/ZombieMace.java
@@ -0,0 +1,11 @@
+package game;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ZombieMace extends WeaponItem{
+
+	public ZombieMace() {
+		super("ZombieMace", 'M', 60, "ZombieMace whacks");
+	}
+
+}
