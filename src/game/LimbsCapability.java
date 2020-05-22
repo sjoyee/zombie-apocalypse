6ee7333f05ecf69750836b548a46fd6d2350538a
Lua Shi Liang <slua0003@student.monash.edu>
2020-05-22 14:16:52 +0800

    Added documentation to LimbsCapability class

--- src/game/LimbsCapability.java
+++ src/game/LimbsCapability.java
@@ -1,7 +1,18 @@
 package game;
 
+/**An enum representing a collection of constants which indicate the capabilities of a Limb.
+ * 
+ * @author Lua Shi Liang
+ *
+ */
 public enum LimbsCapability {
+	/**
+	 * A type of limb which is the arm
+	 */
 	ARM,
+	/**
+	 * A type of limb which is the leg
+	 */
 	LEG
 
 
