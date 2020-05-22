7c3c630b4ae922e88aabbd513e6ca7450f8948e7
Lua Shi Liang <slua0003@student.monash.edu>
2020-05-22 16:08:09 +0800

    Added documentation to PickUpWeaponBehaviour

--- src/game/behaviours/PickUpWeaponBehaviour.java
+++ src/game/behaviours/PickUpWeaponBehaviour.java
@@ -8,9 +8,22 @@ import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.PickUpItemAction;
 import game.Behaviour;
 
+/**
+ * A class that generates a PickUpItemAction if the current Actor is standing on a
+ * location that has a weapon.
+ * 
+ * @author Lua Shi Liang 
+ *
+ */
 public class PickUpWeaponBehaviour implements Behaviour{
 
 
+	/**Returns a PickUpItemAction if there is an WeaponItem on the location it's standing
+	 * 
+     * @param actor the actor having this behaviour
+     * @param map the map where the current actor is
+     * @return a new PickUpIteamAction if conditions are met, else null
+     */
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		for (Item item : map.locationOf(actor).getItems())
