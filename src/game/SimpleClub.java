206568765e72f8943ed0930cfbe49ab532f3c535
Lua Shi Liang <slua0003@student.monash.edu>
2020-05-22 15:04:37 +0800

    Added documentation for SimpleClub and Zombie class

--- src/game/SimpleClub.java
+++ src/game/SimpleClub.java
@@ -3,20 +3,23 @@ package game;
 import edu.monash.fit2099.engine.WeaponItem;
 import game.actions.CraftingAction;
 
+/** A type of Weapon Item that only appears when a Zombie dropped its limb
+ * 
+ * @author Lua Shi Liang
+ *
+ */
 public class SimpleClub extends WeaponItem{
 
-	public SimpleClub(char display) {
+	/** Create a SimpleClub using a char that specifies its display character for SimpleClub, and adding the capability
+	 * @param display the character to use to represent this item if it is on the ground
+	 */
+	public SimpleClub(char display){
 		super("SimpleClub", display, 20, "SimpleClub whacks");
+//		if(display != 'A' || display != 'L') {
+//			throw new Exception("display must be either A or L");
+//		}
 		this.addCapability(ItemCapability.CRAFTABLE);
-//		setLimbsCapability(displayChar);
 	}
-//	private void setLimbsCapability(char displayChar) {
-//		if(displayChar == 'A') {
-//			this.addCapability(LimbsCapability.ARM);
-//		}
-//		else if(displayChar == 'l') {
-//			this.addCapability(LimbsCapability.LEG);
-//		}
-//	}
+
 
 }
--- src/game/Zombie.java
+++ src/game/Zombie.java
@@ -23,23 +23,46 @@ import game.behaviours.WanderBehaviour;
  *
  */
 public class Zombie extends ZombieActor {
+	/**
+	 * An array of type Behaviour which consists of the behaviours of a Zombie.
+	 */
 	private Behaviour[] behaviours = {
 			new AttackBehaviour(ZombieCapability.ALIVE),
 			new HuntBehaviour(Human.class, 10),
 			new PickUpWeaponBehaviour(),
 			new WanderBehaviour()
 	};
+	/**
+	 * An array of type Behaviour which consists of the behaviours of a Zombie without legs.
+	 */
 	private Behaviour[] behavioursWithoutLegs = {new AttackBehaviour(ZombieCapability.ALIVE), new PickUpWeaponBehaviour()};
+	/**
+	 * The number of arms of the zombie
+	 */
 	private int numOfArms = 2;
+	/**
+	 * The number of legs of the zombie
+	 */
 	private int numOfLegs = 2;
+	/**
+	 * The probaility of the zombie using bite or punch
+	 */
 	private double probability = 0.5;
+	/**
+	 * A boolean that indicates whether the zombie can move if it loses a leg
+	 */
 	private boolean isSecondTurn = false;
 
+	
 	public Zombie(String name) {
 		super(name, 'Z', 100, ZombieCapability.UNDEAD);
 	}
 
 	
+	/**This method changes the probability of biting and punching depending on the number 
+	 * of arms it has and drops its weapon if the conditions met
+	 * @param map The map where the zombie currently is
+	 */
 	private void checkArmStatus(GameMap map) {
 		if (numOfArms == 0){
 			probability = 1;
@@ -54,11 +77,15 @@ public class Zombie extends ZombieActor {
 		}
 	}
 	
+	/**
+	 * New feature is added to this overriding method
+	 * if it's hurt, it has 50% of chances of losing a limb
+	 */
 	@Override
 	public void hurt(int damage) {
 		super.hurt(damage);
 		double x = Math.random();
-		if(x < 1) {
+		if(x < 0.5) {
 			Random rand = new Random();
 			if(rand.nextBoolean() && numOfLegs > 0){
 				numOfLegs -=1;
@@ -73,6 +100,12 @@ public class Zombie extends ZombieActor {
 			}
 	}
 
+	/** Returns the action to be perform depending on the array of behaviour it currently has
+	 * 
+	 * @param behaviourArray The array of behaviours that the zombie currently have
+	 * @param map the map where the current Zombie is
+	 * @return the Action to be performed
+	 */
 	private Action returnAction(Behaviour[] behaviourArray, GameMap map){
 		for (Behaviour behaviour : behaviourArray) {
 			Action action = behaviour.getAction(this, map);
@@ -88,8 +121,11 @@ public class Zombie extends ZombieActor {
 	
 
 
+	/**
+	 *Gets 1 of the possible type of intrinsic weapon which are punch or bite.
+	 */
 	@Override
-	public IntrinsicWeapon getIntrinsicWeapon() {  //Changes for Zombie Attacks
+	public IntrinsicWeapon getIntrinsicWeapon() {  
 		double chances = Math.random();
 		if (chances <= probability) {
 			return new IntrinsicWeapon(20,"bites");
@@ -99,12 +135,19 @@ public class Zombie extends ZombieActor {
 
 	/**
 	 * If a Zombie can attack, it will.  If not, it will chase any human within 10 spaces.  
-	 * If no humans are close enough it will wander randomly.
+	 * If no humans are close enough, it will pick up any weapons that are on its location
+	 * If there is no weapons, it will wander randomly.
+	 * 
+	 * if it has no legs, it can only attack or pick up weapons.
+	 * 
+	 * If it drops a limb, a Simpleclub will be drop on its location
 	 * 
 	 * @param actions list of possible Actions
 	 * @param lastAction previous Action, if it was a multiturn action
 	 * @param map the map where the current Zombie is
 	 * @param display the Display where the Zombie's utterances will be displayed
+	 * 
+	 * @return the Action to be performed
 	 */
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
