17af5d68f6a9730530fde08c30337943372811dd
Lua Shi Liang <slua0003@student.monash.edu>
2020-06-10 19:38:47 +0800

    VanishAction class added

---
+++ src/game/actions/VanishAction.java
@@ -0,0 +1,25 @@
+package game.actions;
+
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import game.ZombieCapability;
+
+public class VanishAction extends Action{
+
+	@Override
+	public String execute(Actor actor, GameMap map) {
+		String result = actor + " has vanish!";
+		actor.addCapability(ZombieCapability.VANISH);
+		map.removeActor(actor);
+		return result;
+	}
+
+	@Override
+	public String menuDescription(Actor actor) {
+		return actor + " can vanosh. ";
+	}
+	
+
+}
