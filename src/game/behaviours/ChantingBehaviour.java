25440765d76b43e0e8efb96369cf2c108633ebec
Lua Shi Liang <slua0003@student.monash.edu>
2020-06-13 07:06:37 +0800

    added documentation to chanting behaviour class

--- src/game/behaviours/ChantingBehaviour.java
+++ src/game/behaviours/ChantingBehaviour.java
@@ -7,8 +7,20 @@ import game.Behaviour;
 import game.ZombieCapability;
 import game.actions.SummonZombieAction;
 
+/** 
+ * A class that generates a SummonZombieAction if the current Actor has
+ * a ZombieCapability.CHANT capability
+ * 
+ * @author Lua Shi Liang 
+ *
+ */
 public class ChantingBehaviour implements Behaviour{
 
+	/**
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return a SummonZombieAction if conditions are met
+	 */
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
 		if(actor.hasCapability(ZombieCapability.CHANT)) {
